4887212d5f182115c74a16672a87eac082475296
idaugherty <idaugherty@murraystate.edu>
2025-08-25 09:42:39 -0400

    Created Hello world project

---
+++ HelloWorld.java
@@ -0,0 +1,1 @@
+public class HelloWorld { String name; public HelloWorld(String name) { this.name = name; } public void greet() { System.out.println("Hello, " + name + "!"); } public static void main(String[] args) { HelloWorld student = new HelloWorld("Jason"); student.greet(); } }
